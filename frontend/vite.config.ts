ef8daf27eb6e744352e8e0c8b4c4e33e266a319a
sumanthdynpro <sumanth.m@dynpro.in>
2025-11-12 10:51:53 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -34,10 +34,11 @@ export default defineConfig({
     },
   },
   server: {
+    host: '0.0.0.0',
     port: 3000,
     https: {
-      key: fs.readFileSync('key.pem'),
-      cert: fs.readFileSync('cert.pem'),
+      key: fs.readFileSync('./key.pem'),
+      cert: fs.readFileSync('./cert.pem'),
     },
     proxy: {
       '/api': {
